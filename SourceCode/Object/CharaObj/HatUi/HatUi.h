7e1b6f0dd348c64ce3f47a1ed7d6cc6b4325c532
406 <>
2024-12-06 13:39:08 +0900

    HatUiの実装

---
+++ SourceCode/Object/CharaObj/HatUi/HatUi.h
@@ -0,0 +1,68 @@
+#pragma once
+#include<memory>
+
+namespace object
+{
+	/// <summary>
+	/// 回避中のUi関連
+	/// </summary>
+	class HatUi final
+	{
+	public:
+
+		/// <summary>
+		/// 初期化
+		/// </summary>
+		static void Initialize();
+
+		/// <summary>
+		/// Uiの描画
+		/// </summary>
+		static void DrawUi();
+
+		/// <summary>
+		/// 移動処理
+		/// </summary>
+		static void MoveUi();
+
+		/// <summary>
+		/// 移動値セット
+		/// </summary>
+		static void SetMove();
+
+		/// <summary>
+		/// UiのMove状態取得
+		/// </summary>
+		/// <returns></returns>
+		static const bool GetIsMoveDone() { return hatui->m_IsMoveDone; }
+
+		/// <summary>
+		/// デストラクタ
+		/// </summary>
+		~HatUi();
+
+	private:
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		HatUi();
+
+		/// <summary>
+		/// Uiの読み込み
+		/// </summary>
+		static void LoadUi();
+
+		POINTFLOAT m_UiPos;		//Ui座標
+
+		int m_UiHandle;		//Uiハンドル
+
+		bool m_IsMoveDone;		//Uiの動きが完了したか
+		bool m_IsMoveDown;		//下に動き処理するか
+
+		static const int m_UIMOVE_POS = -320;		//Ui移動座標
+		const float m_UIMOVE_SPEED = 40.0f;			//Ui移動速度
+
+		static std::unique_ptr<HatUi> hatui;	//自身の実態
+	};
+}
