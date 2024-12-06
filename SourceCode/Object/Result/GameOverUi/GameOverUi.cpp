3af9b08ec838e91f55d5db8e3bd3aab9b8863ab3
406 <>
2024-12-06 16:33:08 +0900

    GameOverUiの修正

--- SourceCode/Object/Result/GameOverUi/GameOverUi.cpp
+++ SourceCode/Object/Result/GameOverUi/GameOverUi.cpp
@@ -58,7 +58,7 @@ namespace object
 				{
 					m_hitstatus = "0Hit";
 					m_Uipos.x = m_SelectUi_Pos[0].x;
-					m_Uipos.x = m_SelectUi_Pos[0].y;
+					m_Uipos.y = m_SelectUi_Pos[0].y;
 				}
 				else if (i == 1)//ƒ^ƒCƒgƒ‹‚É–ß‚é‚È‚ç
 				{
