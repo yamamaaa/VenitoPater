c0ffff55c76719ec76336efa248ecae965052b33
yama <Reo953193@gmail.com>
2025-01-20 22:36:38 +0900

    NumDeysの追加実装

--- SourceCode/Object/NumDeys/NumDeys.cpp
+++ SourceCode/Object/NumDeys/NumDeys.cpp
@@ -1,3 +1,4 @@
+#include <Dxlib.h>
 #include "NumDeys.h"
 
 namespace object
@@ -28,12 +29,38 @@ namespace object
     void NumDeys::LoadObject()
     {
         //日数の初期化
-        numdeys->m_NumDeys = 1;
+        numdeys->m_NumDeys = 0;
+        numdeys->m_IsFadeDone = false;
     }
 
     void NumDeys::UpdateNumDeys()
     {
         //一日経過
-        numdeys->m_NumDeys ++;
+        numdeys->m_NumDeys++;
+    }
+
+    void NumDeys::DrawNumDeys()
+    {
+        if (!numdeys->m_IsFadeDone)
+        {
+            numdeys->m_DrawCount += 0.1f;
+
+            //現在の日数を表示
+            SetFontSize(120);
+            DrawFormatString(820, 480, GetColor(numdeys->m_Collar, numdeys->m_Collar, numdeys->m_Collar), "%d日目", numdeys->m_NumDeys);
+
+            SetFontSize(16);
+            DrawFormatString(750, 350, GetColor(255, 255, 255), "日数表示カウント:%f 最高値100", numdeys->m_DrawCount);
+
+            if (numdeys->m_DrawCount >= 100.0f)
+            {
+                numdeys->m_Collar -= m_DROPSPEED;
+
+                if (numdeys->m_Collar <= 0.0f)
+                {
+                    numdeys->m_IsFadeDone = true;	//処理の完了
+                }
+            }
+        }
     }
 }
