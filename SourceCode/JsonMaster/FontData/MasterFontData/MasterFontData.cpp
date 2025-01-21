5a23965b04c97f471c081f8b3142f36dffb3d45d
yama <Reo953193@gmail.com>
2025-01-21 19:56:07 +0900

    FontDataの実装

---
+++ SourceCode/JsonMaster/FontData/MasterFontData/MasterFontData.cpp
@@ -0,0 +1,20 @@
+#include "MasterFontData.h"
+
+#include "../../IOUtility/IOUtility.h"
+
+namespace json_fontdata
+{
+	MasterFontData::MasterFontData()
+		:ioutility(new jsonmaster::IOUtility)
+	{
+		//ファイルを読み込みインスタンス生成
+		Json storyjson = ioutility->Analysis("../SourceCode/JsonMaster/JsonFile/Font/story.json");
+		storydata.reset(new StoryData(storyjson["story"]));
+	}
+
+	MasterFontData::~MasterFontData()
+	{
+		//メモリ解放
+		delete ioutility;
+	}
+}
