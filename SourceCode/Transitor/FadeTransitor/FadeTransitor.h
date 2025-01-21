61194ac0560db5d194d6270157f260974988d8ed
yama <Reo953193@gmail.com>
2025-01-21 19:22:15 +0900

    FadeTransitorの追加実装

--- SourceCode/Transitor/FadeTransitor/FadeTransitor.h
+++ SourceCode/Transitor/FadeTransitor/FadeTransitor.h
@@ -34,6 +34,12 @@ namespace transitor
 		/// <returns>true:完了|false:未完成</returns>
 		const bool IsFadeDone() const { return m_IsFadeDone; }
 
+		/// <summary>
+		/// フェード後処理
+		/// </summary>
+		/// <returns></returns>
+		const void FadeProcessing();
+
 	private:
 
 		/// <summary>
@@ -46,16 +52,20 @@ namespace transitor
 		/// </summary>
 		void LoadObject();
 
-		const int m_DROPSPEED = 15;		//減分速度
+		const float m_DROPSPEED = 5.0f;				//減分速度
+		const float m_RISESPEED = 0.01f;			//増化速度
+
 		const int m_COLLARCODE = 255;	//カラーコード
 
 		const float m_WAITMAX = 50.0f;			//待ち時間
 		const float m_COUNT_DECREMENT = 1.0f;	//カウント減分値
 
-		int m_DropCollar;				//減分カラーコード
+		int m_Collar;					//カラーコード
+		float m_Calculation;			//算出
 
 		bool m_IsFadeDone;				//処理が終わったか
 		bool m_WaitDone;				//一定時間待ったか
+		bool m_CanFade;					//フェード開始できるか
 
 		float m_WaitCount;				//一定時間カウント
 	};
