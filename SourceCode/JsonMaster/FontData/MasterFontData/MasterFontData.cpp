05989e244ab4fc4417e88c021ac32f9eca04dc99
406-07\student <k>
2025-01-25 16:30:37 +0900

    フォントデータの読み込みを修正

--- SourceCode/JsonMaster/FontData/MasterFontData/MasterFontData.cpp
+++ SourceCode/JsonMaster/FontData/MasterFontData/MasterFontData.cpp
@@ -10,6 +10,7 @@ namespace json_fontdata
 		//ファイルを読み込みインスタンス生成
 		Json storyjson = ioutility->Analysis("../SourceCode/JsonMaster/JsonFile/Font/story.json");
 		storydata.reset(new StoryData(storyjson["story"]));
+		//../SourceCode/JsonMaster/JsonFile/Font/story.json
 	}
 
 	MasterFontData::~MasterFontData()
